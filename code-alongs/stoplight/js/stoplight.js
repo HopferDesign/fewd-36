c4cced4394a986c39a9f6222e0aef0d0e1c07fdc
Ben Hopfer <hopferdesign@gmail.com>
2015-10-28 21:12:39 -0700

    added stoplight

---
+++ code-alongs/stoplight/js/stoplight.js
@@ -0,0 +1,24 @@
+var stop = document.querySelector(".stop");
+var slow = document.querySelector(".slow");
+var go = document.querySelector(".go");
+var red = document.querySelector(".red");
+var yellow = document.querySelector(".yellow");
+var green = document.querySelector(".green");
+var hide = document.querySelector(".hidden")
+
+stop.addEventListener("click", changeColor);
+slow.addEventListener("click", changeColor);
+go.addEventListener("click", changeColor);
+
+function changeColor(event) {
+	
+	
+	// var swatch = event.target;
+	// var color = swatch.className;
+	
+	// var body = document.querySelector("body");
+	// body.className = color;
+
+	// var name = document.querySelector("span");
+	// name.textContent = color;
+}
